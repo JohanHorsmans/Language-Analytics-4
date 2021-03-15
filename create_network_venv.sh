6f22b24929ce6d48a1bdc5b71681f53d75758dc0
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-03-15 18:01:04 +0100

    Update create_network_venv.sh

--- create_network_venv.sh
+++ create_network_venv.sh
@@ -11,7 +11,7 @@ pip install jupyter
 
 python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements_network.txt && pip install -r requirements.txt
+test -f requirements.txt && pip install -r requirements.txt
 
 deactivate
 echo "build $VENVNAME"
